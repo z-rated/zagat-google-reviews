36b2236ab9bb2260bec4311c7374411ccb8cccec
Xin Chen <xinchen921005@gmail.com>
2019-06-06 10:59:51 -0700

    changed dataGeneration script to generate 50M records

--- dataGenerator.js
+++ dataGenerator.js
@@ -1,35 +1,42 @@
 const faker = require('faker');
+const Promise = require('bluebird');
+const path = require('path');
+
+var fs = require('fs');
 
 
 
-var fs = require('fs')
-
-
-
-
-var output = fs.createWriteStream('data.csv.gz');
+var output = fs.createWriteStream(path.resolve('data.csv.gz'));
 var zlib = require('zlib');
+
+var file = fs.createWriteStream(path.resolve('file.csv'));
 var compress = zlib.createGzip();
 compress.pipe(output);
-for(let i = 1; i <= 10000000; i++) {
-  let text = 
-            i + ',' 
-            + faker.name.findName() + ',' 
-            + faker.image.imageUrl() + ',' 
-            + faker.date.past() + ',' +
-            + faker.finance.amount(0, 5, 1) + ',' 
-            + faker.lorem.paragraph() + ',' 
-            + faker.random.number(100000);
-  compress.write(text+'\n');
+
+
+//modified to generate 10M restaurants' reivews and each restaurant has 5 reviews
+ //compress.write('review_id,reviewer,picture,date_posted,rating,text_review,rest_id');
+
+async function createAnyLinesYouWant(writer, lines) {
+  for(let i = 1; i <= lines; i++) {
+    const text = 
+              i + ',' 
+              + faker.name.findName() + ',' 
+              + faker.image.imageUrl() + ',' 
+              + faker.date.past() + ','
+              + faker.finance.amount(0, 5, 1) + ',' 
+              + faker.lorem.paragraph() + ',' 
+              + Math.floor((i-1)/5 + 1);
+    const ok = writer.write('\n' + text);
+    if(!ok) {
+      await new Promise(resolve => {
+        writer.once('drain', resolve);
+      })
+    }
+    if(i === lines) {
+      writer.end();
+    }
+  }
 }
-
-compress.end();
-compress.on('finish', () => {
-  console.log('...done')
-});
-
-compress.on('error', function(err) {
-  console.log(err.stack);
-});
-
+createAnyLinesYouWant(compress, 50000000);
 
