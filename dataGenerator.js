0931c622392c3864122204f908a66ec4f6d6d5c4
Xin Chen <xinchen921005@gmail.com>
2019-06-10 11:34:09 -0700

    modified script to generate 2 more tables and json data

--- dataGenerator.js
+++ dataGenerator.js
@@ -42,7 +42,7 @@ async function writeData(reviewWriter, restaurantWriter, userWriter, number) {
                   + faker.finance.amount(0, 5, 1) + ',' 
                   + faker.lorem.paragraph() + ',' 
                   + i + ','
-                  + (number - i + 1);
+                  + (1+(((i - 1) * 5 + j - 1) % number));
 
       const ableWriteReview = reviewWriter.write('\n' + review);
       if(!ableWriteReview) {
@@ -65,5 +65,72 @@ async function writeData(reviewWriter, restaurantWriter, userWriter, number) {
     }
   }
 }
-writeData(reviewWriteStream,restaurantWriteStream, userWriteStream, 10000000);
+//writeData(reviewWriteStream,restaurantWriteStream, userWriteStream, 10000000);
+var restaurantJsonWriteStream = fs.createWriteStream(path.resolve('restaurants.json'));
+async function writeJsonData(restaurantWriter,number) {
+  const start = new Date();
+  for(let i = 1; i <= number; i++) {
+    var obj = {};
+    obj['restaurant_id'] = i;
+    obj['restaurant'] = faker.name.findName()+`'s`;
+    var reviews = [];
+    // const user = (number - i + 1) + ',' + faker.name.findName() + ',' + faker.image.avatar();
+    // const ableWriteUser = userWriter.write('\n'+user);
+    // if(!ableWriteUser) {
+    //   await new Promise(resolve => {
+    //     userWriter.once('drain', resolve);
+    //   })
+    // }
+    for(let j = 1; j <= 5; j++) {
+      // const review = ((i - 1) * 5 + j) + ',' 
+      //             + faker.date.past() + ','
+      //             + faker.finance.amount(0, 5, 1) + ',' 
+      //             + faker.lorem.paragraph() + ',' 
+      //             + i + ','
+      //             + (1+(((i - 1) * 5 + j - 1) % number));
+      var reviewObj = {};
+      var author = {};
+      reviewObj['review_id'] = ((i - 1) * 5 + j);
+      reviewObj['date_posted'] = faker.date.past();
+      reviewObj['rating'] = faker.finance.amount(0, 5, 1);
+      reviewObj['text_review'] = faker.lorem.sentence();
+      author['user_id'] = 1+(((i - 1) * 5 + j - 1) % number);
+      author['username'] = faker.name.findName();
+      author['profile_pic'] = faker.image.avatar();
+      reviewObj['author'] = author;
+      reviews.push(reviewObj);
+      // const ableWriteReview = reviewWriter.write('\n' + review);
+      // if(!ableWriteReview) {
+      //   await new Promise(resolve => {
+      //     reviewWriter.once('drain', resolve);
+      //   })
+      // }
+    }
+    obj['reviews'] = reviews;
+    const ableWriteRes = restaurantWriter.write(JSON.stringify(obj, null, 1));
+    if(!ableWriteRes) {
+      await new Promise(resolve => {
+        restaurantWriter.once('drain', resolve);
+      })
+    }
 
+    if(i === number) {
+      // reviewWriter.end(()=>{
+      //   console.log('End of writing reviews!')
+      // });
+      // userWriter.end(()=>{
+      //   console.log('End of writing users!')
+      // });
+      restaurantWriter.end(()=>{
+        let end = new Date();
+        console.log('End of write restaurants!\n' + `It took ${(end - start)/60000}mins to generate data of ${number} primary items`);
+      });
+    }
+  }
+}
+writeJsonData(restaurantJsonWriteStream,10000000);
+
+
+
+
+//select review_id,date_posted,rating,restaurant_id,user_id,username,profile_pic,text_review from reviews inner join users on (reviews.restaurant_id = 1 and reviews.user_id = users.id);
--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,3 @@
-
-
-DROP TABLE IF EXISTS reviews;
-
-CREATE TABLE reviews (
-  review_id INT,
-  date_posted VARCHAR(255) NOT NULL,
-  rating DECIMAL NOT NULL,
-  text_review TEXT NOT NULL,
-  restaurant_id INT REFERENCES restaurants(id),
-  user_id INT REFERENCES users(id),
-  PRIMARY KEY (review_id, rest_id, user_id)
-);
-
-
 DROP TABLE IF EXISTS restaurants;
 
 CREATE TABLE restaurants (
@@ -30,10 +15,26 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews (
+  review_id INT,
+  date_posted VARCHAR(255) NOT NULL,
+  rating DECIMAL NOT NULL,
+  text_review TEXT NOT NULL,
+  restaurant_id INT REFERENCES restaurants(id),
+  user_id INT REFERENCES users(id),
+  PRIMARY KEY (review_id, restaurant_id, user_id)
+);
+
+
+
 /* MongoDB Schema 
   collection : restaurants
   {
     _id: ObjectId(...);
+    name: 'some name';
     reviews: [
       {
         posted_date: "January 10, 2019",
@@ -57,6 +58,7 @@ CREATE TABLE users (
         rating: "4.5",
         text_review: "Awesome!"
         author: {
+          id: '12345'
           username: "Xin Chen",
           profile_pic: "some link";
           }
